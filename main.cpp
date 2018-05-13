ef906f5329199112f2dc975ffd92de36d493ca40
osamu <tarekenroom@gmail.com>
2018-05-13 20:00:16 +0900

    init

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    for (int i = 0; i < a; i++)
+    {
+        // 行
+        for (int j = 0; j < b; j++)
+        {
+            // 列
+            /* code */
+            int c;
+            cin >> c;
+        }
+        /* code */
+    }
+    
+    for(int i = 0; i < a; i++)
+    {
+        // 行
+        /* code */
+    }
+    
+    return 0;
+}
